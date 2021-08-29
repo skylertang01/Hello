f9f9813a7a068a90edec49f5cad33b3243c690f9
Skyler Tang <stang10@huskers.unl.edu>
2021-08-29 19:44:09 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Skyler Tang
+ * Date: 2021/08/24
+ *
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Hello World!\n");
+
+     return 0;
+ }
