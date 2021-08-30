f2fc5b505c67ea86ca79bbbc87703ad870c2ab60
quand01 <89498237+quand01@users.noreply.github.com>
2021-08-30 17:11:01 -0500

    added partner's name/major

--- hello.c
+++ hello.c
@@ -12,7 +12,7 @@
      printf("Skyler Tang!\n");
      printf("Major: Computer Science\n");
      printf("Quan Do\n");
-     printf("Major: Computer Science\n;
+     printf("Major: Computer Science\n);
 
 
      return 0;
